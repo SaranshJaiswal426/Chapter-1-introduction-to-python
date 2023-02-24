5943482db311667de6ecf4f06c20dee2058fab5b
saranshjaiswal426 <Co.2019.sdjaiswal@bitwardha.ac.in>
2023-02-24 13:30:04 +0530

    enter variable and assign value

---
+++ assign variable nd values.py
@@ -0,0 +1,35 @@
+b = 9223372036854775807
+print(type(b))
+# Output: <type 'int'>
+pi = 3.14
+print(type(pi))
+# Output: <type 'float'>
+c = 'A'
+print(type(c))
+# Output: <type 'str'>
+name = 'John Doe'
+print(type(name))
+# Output: <tyb = 9223372036854775807
+print(type(b))
+# Output: <type 'int'>
+pi = 3.14
+print(type(pi))
+# Output: <type 'float'>
+c = 'A'
+print(type(c))
+# Output: <type 'str'>
+name = 'John Doe'
+print(type(name))
+# Output: <type 'str'>
+q = True
+print(type(q))
+# Output: <type 'bool'>
+x = None
+print(type(x))
+# Output: <type 'NoneType'>pe 'str'>
+q = True
+print(type(q))
+# Output: <type 'bool'>
+x = None
+print(type(x))
+# Output: <type 'NoneType'>

aa78b3c28abe2e69984c15865f4cde73e0b2fd7b
saranshjaiswal426 <Co.2019.sdjaiswal@bitwardha.ac.in>
2023-02-24 13:38:44 +0530

    block implement

---
+++ block.py
@@ -0,0 +1,10 @@
+a = 2
+b = 3
+
+if a > b: # If block starts here
+ print(a) # This is part of the if block
+else: # else must be at the same level as if
+ print(b) # This line is part of the else block
+print('next code')
+if a < b: print(a)
+else: print(b)
